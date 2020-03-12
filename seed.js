fcefe8c01274a949e6a9031c9369fd5a082accb5
jim walter <jimwalter13090@gmail.com>
2020-03-12 11:47:17 -0600

    API requesting movie information using ignored API config file

--- seed.js
+++ seed.js
@@ -1,11 +1,37 @@
 const Axios = require('axios');
 const connection = require('./db/index.js');
+const { API_KEY } = require('./db/config.js');
 
-const getMoviesAndPopulateDB = function() {
-    let movieIds = [];
-    let moviesInfo = [];
+async function getMoviesAndPopulateDB() {
+  let movieIds = [];
+  let moviesInfo = [];
 
-    Axios.get('https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&sort_by=popularity.desc&include_video=false&page=${page}')
-}
+  let page = 1;
+
+  while (page <= 5) {
+
+    let discoverString = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_video=false&page=${page}`;
+
+    await Axios.get(discoverString)
+      .then(results => {
+        let movies = results.data.results;
+        movies.forEach(movie => {
+          movieIds.push(movie.id);
+        });
+      })
+      .catch(err => console.log(err));
+
+      page++;
+  };
+
+  movieIds.forEach(movie => {
+    let movieString = `https://api.themoviedb.org/3/movie/${movie}?api_key=${API_KEY}&language=en-US`;
+    //   console.log(movieString);
+    await Axios.get(movieString)
+      .then(movieResults => console.log(movieResults))
+      .catch(err => console.log(err));
+  });
+
+};
 
 getMoviesAndPopulateDB();
