492465f1a8fdd6a4bd68590ade783979b6d80c97
jim walter <jimwalter13090@gmail.com>
2020-03-13 18:46:57 -0600

    styling updates

--- client/src/components/Rating.jsx
+++ client/src/components/Rating.jsx
@@ -5,11 +5,13 @@ import { Rating } from '@material-ui/lab';
 const RatingBar = (props) => {
 
   return (
-    <div className="ratings">
-      <div>
-        <Rating name="read-only" value={props.stars || 0} precision={0.1} readOnly />
-      </div>
-      <span className="marginLeft"><p>IMDb {props.imdb}</p></span>
+    <div className="ratings flexRow">
+      <span>
+        <Rating name="read-only" value={props.stars || 0} precision={0.01} readOnly />
+      </span>
+      <span className="marginLeft"><p id="vote">({props.votes})</p></span>
+      <span className="marginLeft"><img className="imdb" src="https://ia.media-imdb.com/images/M/MV5BMTk3ODA4Mjc0NF5BMl5BcG5nXkFtZTgwNDc1MzQ2OTE@._V1_.png" /></span>
+      <span className="marginLeft"><p>{props.imdb}</p></span>
       <span className="marginLeft"><p>{props.duration} min</p></span>
     </div>
   );
