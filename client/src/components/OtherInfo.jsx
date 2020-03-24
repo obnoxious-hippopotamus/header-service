67152b0e4f21636a8c13af9971352a9fa63cbace
jim walter <jimwalter13090@gmail.com>
2020-03-24 16:54:08 -0600

    updated styling

--- client/src/components/OtherInfo.jsx
+++ client/src/components/OtherInfo.jsx
@@ -4,9 +4,9 @@ import React from 'React';
 const OtherInfo = (props) => {
   return (
     <div>
-      <h3 className="flexRow">Genres<p>{props.genres}</p></h3>
-      <h3 className="flexRow">Director<p>{props.director}</p></h3>
-      <h3 className="flexRow">Starring<p>{props.starring}</p></h3>
+      <h3 className="flexRow">Genres<p className="extra">{props.genres}</p></h3>
+      <h3 className="flexRow">Director<p className="extra">{props.director}</p></h3>
+      <h3 className="flexRow">Starring<p className="extra">{props.starring}</p></h3>
     </div>
   );
 };
